8d16d8f8bbe4f38fea22de5ff1df43adad719b22
Mikhail Knyazev <solnseselo@yandex.ru>
2023-12-04 16:03:04 +0400

    3 task.

--- src/main/java/com/example/queueprocessorapp3/processor/AddEmployeeProcessor.java
+++ src/main/java/com/example/queueprocessorapp3/processor/AddEmployeeProcessor.java
@@ -2,24 +2,26 @@ package com.example.queueprocessorapp3.processor;
 
 import com.example.queueprocessorapp3.service.EmployeeService;
 import com.example.queueprocessorapp3.entity.Employee;
+import com.example.queueprocessorapp3.converter.EmployeeConverter;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
-import java.time.Instant;
 
 @Component
 public class AddEmployeeProcessor implements Processor {
 
     private final EmployeeService employeeService;
     private final ObjectMapper objectMapper;
+    private final EmployeeConverter employeeConverter;
 
     @Autowired
-    public AddEmployeeProcessor(EmployeeService employeeService, ObjectMapper objectMapper) {
+    public AddEmployeeProcessor(EmployeeService employeeService, ObjectMapper objectMapper, EmployeeConverter employeeConverter) {
         this.employeeService = employeeService;
         this.objectMapper = objectMapper;
+        this.employeeConverter = employeeConverter;
     }
 
     @Override
@@ -27,14 +29,7 @@ public class AddEmployeeProcessor implements Processor {
         String json = exchange.getIn().getBody(String.class);
         JsonNode jsonNode = objectMapper.readTree(json);
 
-        Employee employee = new Employee();
-        // Предполагается, что у класса Employee есть соответствующие сеттеры
-        employee.setFirstName(jsonNode.get("firstName").asText());
-        employee.setLastName(jsonNode.get("lastName").asText());
-        employee.setAge(jsonNode.get("age").asInt());
-        employee.setProfession(jsonNode.get("profession").asText());
-        employee.setHandledTimestamp(Instant.now());
-        employee.setStatus("Complete");
+        Employee employee = employeeConverter.convert(jsonNode);
 
         // Добавление или обновление сотрудника в базе данных
         Long employeeId = employeeService.addOrUpdateEmployee(employee);
